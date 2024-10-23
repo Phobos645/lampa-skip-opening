c4849bc9f7c338dff787d1777f1e6c54c3bb1a8b
Phobos645 <amelinkirill2005@gmail.com>
2024-10-23 14:25:20 +0600

    Updated skip_opening.js to skip forward 85 seconds

--- skip_opening.js
+++ skip_opening.js
@@ -1,38 +1,45 @@
 Lampa.Platform.tv();
 function log() {
-        console.log.apply(console.log, arguments);
-      }
-log('Skip Opening', 'Plugin loaded for skipping openings');
+    console.log.apply(console.log, arguments);
+}
 
-function seekForward(seconds) {
-    const video = Lampa.Player.video();
-    if (video) {
-        const newTime = video.currentTime + seconds;
-        video.currentTime = newTime > video.duration ? video.duration : newTime; // Не перематывать дальше конца
-        log('Skip Opening', `Skipped forward by ${seconds} seconds`);
-    } else {
-        log('Skip Opening', 'Video not found');
+log('Skip Opening Button with Custom Icon', 'Plugin loaded to add a skip opening button with a custom icon');
+
+// Функция для перемотки на 85 секунд вперед
+function skipOpening() {
+    if (Lampa.Player.video) {
+        let currentTime = Lampa.Player.video.time; // Получаем текущее время
+        Lampa.Player.video.time = currentTime + 85; // Увеличиваем его на 85 секунд
+        log('Skip Opening', 'Skipped forward 85 seconds');
     }
 }
 
-function listenDestroy() {
-	document.removeEventListener("keydown", listenHotkeys);
-	Lampa.Player.listener.remove('destroy', listenDestroy);	
-}
+// Функция для создания кнопки и добавления её в интерфейс плеера
+function addSkipButton() {
+    // Проверяем, существует ли панель плеера
+    let playerControls = document.querySelector('.player-panel');
+    
+    if (playerControls) {
+        // Создаем новую кнопку
+        let skipButton = document.createElement('div');
+        skipButton.classList.add('player-panel__button', 'button', 'selector');
+        
+        // Вставляем твою SVG-иконку
+        skipButton.innerHTML = `
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 455 455" style="enable-background:new 0 0 455 455;" xml:space="preserve">
+                <path d="M227.5,0C101.855,0,0,101.855,0,227.5S101.855,455,227.5,455S455,353.145,455,227.5S353.145,0,227.5,0z M356.798,330h-55 v-78.981L179.64,321.575v-47.038l-81.438,47.038V133.425l81.438,47.038v-47.038l122.157,70.557V125h55V330z"/>
+            </svg>
+        `;
 
-function startHotkeys() {
-	document.addEventListener("keydown", listenHotkeys);
-	Lampa.Player.listener.follow('destroy', listenDestroy);
-}
+        // Привязываем функцию перемотки к событию нажатия на кнопку
+        skipButton.addEventListener('click', skipOpening);
 
-function listenHotkeys(e) {
-    //log('Hotkeys', e.keyCode);
+        // Добавляем кнопку в панель управления плеером
+        playerControls.appendChild(skipButton);
 
-    // Кнопка 8 для перемотки на 1 минуту 25 секунд (85 секунд)
-    if (e.keyCode === 56 || e.keyCode === 104 || e.keyCode === 9) {
-        log('Skip Opening', '8 pressed');
-        seekForward(85);  // Перематываем на 85 секунд
+        log('Skip Opening', 'Skip button with custom icon added to player controls');
     }
 }
 
-Lampa.Player.listener.follow('ready', startHotkeys);
+// Слушатель для добавления кнопки, когда плеер готов
+Lampa.Player.listener.follow('ready', addSkipButton);
